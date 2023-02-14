e907dafe9d4abdf0fcb6dcdfe2aa41ee675f09bd
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-02-14 19:35:05 +0100

    nuevos cambion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,15 @@ import RadioGroup from '@mui/material/RadioGroup';
 import RadioButton from './components/layout/Utils/Forms/RadioButton'
 import FormLabel from '@mui/material/FormLabel';
 import { ImportantDevices } from '@mui/icons-material';
+import NewCourse  from './components/layout/Forms/NewCourse.jsx';
 
 
 
 function App() {
 
   return (
-    <div className='boxUno'>
-        <RadioButton 
-          sx={{
-            display: "flex",
-            flexDirection:'column',
-            justifyContent: "space-around",
-            color: "black",
-            bgcolor: "#F1F0F0",
-            maxWidth: 300,
-            height: 100,
-            p: 4,
-            
-          }}
-          titleGroup='Seu'
-          Group='Girona'
-          GroupDos='Barcelona'
-          GroupTres='Tarragona'
-          GroupCuatro='Salt'
-        />
+    <div >
+        <NewCourse/>
 
     </div>
 
