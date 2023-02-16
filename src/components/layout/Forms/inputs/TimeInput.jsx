40d47aac8c563c0e04c0f79cc570a4cfb4d8e142
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-02-16 09:08:20 +0100

    new TimeInput

--- src/components/layout/Forms/inputs/TimeInput.jsx
+++ src/components/layout/Forms/inputs/TimeInput.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 
-const TimeInput = () => {
+
+
+export default function TimeInput(props) {
   return (
-    <div>TimeInput</div>
-  )
+    <Stack 
+      component="form" 
+      sx={{
+        displa: "flex",
+        flexDirection: "row",
+        p: 2,
+        label:{color:'green'},
+        '& input': { color: 'black' },
+        TextField:{color:'black'},
+        time:{color:'black'},
+        bgcolor:'white'
+      }}
+    >
+      <TextField
+        id="time"
+        label={props.tituloClock} 
+        type="time"
+        defaultValue="00:00"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{
+          step: 300, // 5 min
+        }}
+        sx={{ width: 110,}}
+        
+      />
+      <Box sx={{ m: 2 }}>{props.de}</Box>
+      <TextField
+        id="time"
+        label={props.tituloClockDos}
+        type="time"
+        defaultValue="07:30"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{
+          step: 300, // 5 min
+        }}
+        sx={{ width: 110 }}
+      />
+    </Stack>
+  );
 }
-
-export default TimeInput
