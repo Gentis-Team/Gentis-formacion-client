db6101a989d5a7652dfed998790ec81526ebc4bd
Kuantize Strigoi <adrian.sunye@gmail.com>
2023-03-01 10:54:32 +0100

    fixed lock.json // fixed principal color mode

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, ButtonGroup, Grid} from '@mui/material';
+import { Box, Button, ButtonGroup, Grid, styled} from '@mui/material';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getFilteredCoursesFn } from '@/api/courseApi';
 import FullScreenLoader from '@/components/layout/loaders/FullScreenLoader';
@@ -13,6 +13,9 @@ import {useQueryLocations, useQueryRequirements, useQueryGroups, useQueryCategor
 import { useFiltersContext } from '@/services/providers/FiltersContextProvider';
 import { getAllCoursesFn } from '../../api/courseApi';
 
+const SButton = styled(Button)({
+  backgroundColor: '#BED730',
+})
 const Home = () => {
  /* A hook that is used to get the courses from the database. */
   const coursesContext = useCoursesContext();
@@ -70,8 +73,8 @@ const Home = () => {
         <Search onSearch={handleOnSearch} onClear={handleOnClear} items={courses} />
       </Box>
       <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button onClick={handleFilterClick('bottom-start')}>Filtra els cursos</Button>
-        <Button onClick={handleFilterClick('bottom-end')}>{filtersContext.state.filters !== null ? filtersContext.state.filters.requirements : 'no va'}</Button>
+        <SButton sx onClick={handleFilterClick('bottom-start')}>Filtra els cursos</SButton>
+        <SButton onClick={handleFilterClick('bottom-end')}>Ordena</SButton>
       </ButtonGroup>
       <FilterPopper handleClose={setOpen} open={open} anchorEl={anchorEl} placement={placement}/>
       {courses?.length === 0 || query?.length === 0 ? (
