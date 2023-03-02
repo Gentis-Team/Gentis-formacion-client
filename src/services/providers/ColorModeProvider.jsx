6bea55771ecce3f5d18148cb501b7a1093ec8916
Camilo Rocca <camiloroccadev@gmail.com>
2023-03-02 19:22:06 +0100

    Modified ColorModeProvider with actual palette, tipography & shape

--- src/services/providers/ColorModeProvider.jsx
+++ src/services/providers/ColorModeProvider.jsx
@@ -2,106 +2,116 @@ import React, { useState, createContext, useContext, useMemo } from "react";
 import { ThemeProvider, createTheme } from '@mui/material';
 
 
-export const ColorModeContext = createContext({
+export const ColorModeContext = createContext( {
     toggleColorMode: () => { }
-});
+} );
 
-export const ColorModeProvider = ({ children }) => {
-    const [mode, setMode] = useState("dark");
+export const ColorModeProvider = ( { children } ) => {
+    const [ mode, setMode ] = useState( "dark" );
     const colorMode = useMemo(
-        () => ({
+        () => ( {
             toggleColorMode: () => {
-                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+                setMode( ( prevMode ) => ( prevMode === 'light' ? 'dark' : 'light' ) );
             },
-        }),
+        } ),
         [],
     );
 
 
     const theme = useMemo(
         () =>
-            createTheme({
+            createTheme( {
                 palette: {
                     mode,
-                    ...(mode === 'light' && {
-                        blue: {
-                            500: '#00545F',
-                            400: '#16727E',
-                            300: '#1E99A9',
-                            200: '#A4E6EF',
-                            100: '#E9F9FB',
-                        },
-                        green: {
-                            500: '#78891A',
-                            400: '#9EB423',
-                            300: '#BED730',
-                            200: '#D9E887',
-                            100: '#F4F8DD',
-                        },
-                        brown: {
-                            500: '#321E1F',
-                            400: '#734547',
-                            300: '#734547',
-                            200: '#D1B3B4',
-                            100: '#F7F2F3',
-                        },
-                        basic: {
-                            red: '#F65447',
-                            green: '#F65447',
+                    ...( mode === 'light' && {
+                        palette: {
+                            type: 'light',
+                            primary: {
+                                main: '#00545f',
+                            },
+                            secondary: {
+                                main: '#bed730',
+                            },
+                            info: {
+                                main: '#9eb424',
+                            },
+                            background: {
+                                default: '#fafafa',
+                            },
                         },
                     } ),
-                    ...(mode === 'dark' && {
-                        blue: {
-                            500: '#00545F',
-                            400: '#16727E',
-                            300: '#1E99A9',
-                            200: '#A4E6EF',
-                            100: '#E9F9FB',
-                        },
-                        green: {
-                            500: '#78891A',
-                            400: '#9EB423',
-                            300: '#BED730',
-                            200: '#D9E887',
-                            100: '#F4F8DD',
-                        },
-                        brown: {
-                            500: '#321E1F',
-                            400: '#734547',
-                            300: '#734547',
-                            200: '#D1B3B4',
-                            100: '#F7F2F3',
-                        },
-                        basic: {
-                            red: '#F65447',
-                            green: '#0A9018',
-                        }                    }),
-                    always: {
-                        alwaysBlack: '#000000',
-                        alwaysWhite: '#FFFFFF',
+                    // ...( mode === 'dark' && {
+
+                    // } ),
+                },
+                typography: {
+                    fontFamily: 'Open Sans',
+                    fontSize: 14,
+                    fontWeightBold: 900,
+                    fontWeightMedium: 600,
+                    fontWeightLight: 500,
+                    h1: {
+                        fontSize: 80,
+                        fontFamily: 'Kanit',
+                        fontWeight: 800,
+                        lineHeight: '5.5rem',
+                        
                     },
-                    
-                }
-            }),
-        [mode],
+                    h2: {
+                        fontFamily: 'Kanit',
+                        fontWeight: 800,
+                        fontSize: '4.5rem',
+                        lineHeight: 1.1,
+                    },
+                    h3: {
+                        fontSize: '3.5rem',
+                        lineHeight: 1.1,
+                        fontWeight: 1000,
+                    },
+                    h4: {
+                        fontSize: '2.5rem',
+                        fontWeight: 1000,
+                    },
+                    h5: {
+                        fontSize: '0.9rem',
+                        lineHeight: 1.2,
+                        fontWeight: 800,
+                    },
+                    h6: {
+                        fontSize: '1.5rem',
+                        fontWeight: 1000,
+                    },
+                    subtitle1: {
+                        fontWeight: 700,
+                    },
+
+                },
+                shape: {
+                    borderRadius: '1rem',
+                },
+            } ),
+        [ mode ],
     );
 
     return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                {children}
+        <ColorModeContext.Provider value={ colorMode }>
+            <ThemeProvider theme={ theme }>
+                { children }
             </ThemeProvider>
         </ColorModeContext.Provider>
     );
 };
 
 const useColorMode = () => {
-    const context = useContext(ColorModeContext);
+    const context = useContext( ColorModeContext );
 
-    if (context === undefined) {
-        throw new Error("useColorMode must be used within a ColorProvider");
+    if ( context === undefined ) {
+        throw new Error( "useColorMode must be used within a ColorProvider" );
     }
     return context;
 };
 
 export default useColorMode;
+
+
+
