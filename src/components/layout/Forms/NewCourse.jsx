96357ccf2abe8c60e2a8af50df51e8df33f7db34
ecp12 <106453089+ecp12@users.noreply.github.com>
2023-03-08 00:45:45 +0100

    create new change

--- src/components/layout/Forms/NewCourse.jsx
+++ src/components/layout/Forms/NewCourse.jsx
@@ -13,99 +13,113 @@ import FormGroup from "@mui/material/FormGroup";
 import DateInput from "../Forms/inputs/DateInput";
 import TimeInput from "../Forms/inputs/TimeInput";
 import Checkboxe from "./inputs/Checkbox";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { createCourseFn } from '@/api/courseApi';
-import {
-  Controller,
-  FormProvider,
-  useForm,
-} from 'react-hook-form';
-import { object, string, z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCourseFn } from "@/api/courseApi";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import { object, string, z } from "zod";
 import CategoriesRadios from "./inputs/CategoriesRadios";
-import { useCategoriesContext } from '@/services/providers/CategoriesContextProvider';
-import { useQueryLocations, useQueryRequirements, useQueryGroups, useQueryCategories } from '@/services/hooks/useQuery';
-import FullScreenLoader from '@/components/layout/loaders/FullScreenLoader';
-import { getAllCategoriesFn } from '@/api/categoryApi';
-import { getAllLocationsFn } from '@/api/locationApi';
-import { useLocationsContext } from '@/services/providers/LocationsContextProvider';
-import { getAllRequirementsFn } from '@/api/requirementApi';
-import { useRequirementsContext } from '@/services/providers/RequirementsContextProvider';
+import { useCategoriesContext } from "@/services/providers/CategoriesContextProvider";
+import {
+  useQueryLocations,
+  useQueryRequirements,
+  useQueryGroups,
+  useQueryCategories,
+} from "@/services/hooks/useQuery";
+import FullScreenLoader from "@/components/layout/loaders/FullScreenLoader";
+import { getAllCategoriesFn } from "@/api/categoryApi";
+import { getAllLocationsFn } from "@/api/locationApi";
+import { useLocationsContext } from "@/services/providers/LocationsContextProvider";
+import { getAllRequirementsFn } from "@/api/requirementApi";
+import { useRequirementsContext } from "@/services/providers/RequirementsContextProvider";
 import { Box } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { useNavigate } from "react-router-dom";
-
-
 
 export default function ColorButtons() {
   const categoriesContext = useCategoriesContext();
   const locationsContext = useLocationsContext();
   const [selectedLocations, setSelectedLocations] = React.useState({});
-  const [selectedRequirements, setSelectedRequiretements] = React.useState('Sense titulacions');
+  const [selectedRequirements, setSelectedRequiretements] =
+    React.useState("Sense titulacions");
   const [selectedCategories, setSelectedCategories] = useState({});
   const [form, setForm] = useState({});
-
 
   const handleChange = (event) => {
     const name = event.target.name;
 
     let value = event.target.value;
-    setForm(values => ({ ...values, [name]: value }));
-  }
+    setForm((values) => ({ ...values, [name]: value }));
+  };
 
   const handleRequirementsChange = (event) => {
     setSelectedRequirements({
       ...selectedRequirements,
       [event.target.name]: event.target.checked,
     });
-  }; 
+  };
 
-  const { isLoading, data: categories } = useQuery(['categories'], () => getAllCategoriesFn(), {
-    select: (data) => data.categories,
-    onSuccess: (data) => {
-      categoriesContext.dispatch({ type: 'SET_CATEGORIES', payload: data });
-    },
-    onError: (error) => useHandleError(error),
-  });
+  const { isLoading, data: categories } = useQuery(
+    ["categories"],
+    () => getAllCategoriesFn(),
+    {
+      select: (data) => data.categories,
+      onSuccess: (data) => {
+        categoriesContext.dispatch({ type: "SET_CATEGORIES", payload: data });
+      },
+      onError: (error) => useHandleError(error),
+    }
+  );
 
-  const { data: locations } = useQuery(['locations'], () => getAllLocationsFn(), {
-    select: (data) => data.locations,
-    onSuccess: (data) => {
-      locationsContext.dispatch({ type: 'SET_LOCATIONS', payload: data });
-    },
-    onError: (error) => useHandleError(error),
-  });
+  const { data: locations } = useQuery(
+    ["locations"],
+    () => getAllLocationsFn(),
+    {
+      select: (data) => data.locations,
+      onSuccess: (data) => {
+        locationsContext.dispatch({ type: "SET_LOCATIONS", payload: data });
+      },
+      onError: (error) => useHandleError(error),
+    }
+  );
 
   const requirementsContext = useRequirementsContext();
   const navigate = useNavigate();
 
-  const { data: requirements } = useQuery(['requirements'], () => getAllRequirementsFn(), {
-    select: (data) => data.requirements,
-    onSuccess: (data) => {
-      requirementsContext.dispatch({ type: 'SET_REQUIREMENTS', payload: data });
-    },
-    onError: (error) => useHandleError(error),
-  });
+  const { data: requirements } = useQuery(
+    ["requirements"],
+    () => getAllRequirementsFn(),
+    {
+      select: (data) => data.requirements,
+      onSuccess: (data) => {
+        requirementsContext.dispatch({
+          type: "SET_REQUIREMENTS",
+          payload: data,
+        });
+      },
+      onError: (error) => useHandleError(error),
+    }
+  );
   const queryClient = useQueryClient();
   const { mutate: createCourse } = useMutation(
     (course) => createCourseFn(course),
     {
       onSuccess: () => {
-        queryClient.invalidateQueries(['courses']);
-        toast.success('Course created successfully');
+        queryClient.invalidateQueries(["courses"]);
+        toast.success("Course created successfully");
       },
       onError: (error) => {
         if (Array.isArray(error.response.data.error)) {
           error.data.error.forEach((el) =>
             toast.error(el.message, {
-              position: 'top-right',
+              position: "top-right",
             })
           );
         } else {
           toast.error(error.response.data.message, {
-            position: 'top-right',
+            position: "top-right",
           });
         }
       },
@@ -113,14 +127,12 @@ export default function ColorButtons() {
   );
 
   const handleCategoriesChange = (event) => {
-    setSelectedCategories((event.target).value);
+    setSelectedCategories(event.target.value);
   };
 
   const handleLocationsChange = (event) => {
-    setSelectedLocations((event.target).value);
+    setSelectedLocations(event.target.value);
   };
-
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -132,8 +144,7 @@ export default function ColorButtons() {
       duration_theory: form.duration_theory,
       duration_practice: form.duration_practice,
       description: form.description,
-    }
-
+    };
 
     createCourse(formData);
   };
@@ -141,32 +152,20 @@ export default function ColorButtons() {
     return <FullScreenLoader />;
   }
   return (
-    <Grid
+    <Box
       justifyContent="center"
       alignItems="center"
       alignContent="center"
       container
       direction="column"
-      sx={{ bgcolor: "white", p: '16px', width: '100%' }}
-
+      sx={{ bgcolor: "white", p: "16px", m: "auto", width: "100%" }}
     >
       <Stack
         justifyContent="center"
         direction="row"
         spacing={2}
-        sx={{ p: 2, m: 2, }}
+        sx={{ p: 2, m: 2 }}
       >
-        <Button
-          variant="contained"
-          sx={{
-            bgcolor: "#BED730",
-            color: "black",
-            borderRadius: "16px",
-            width: 130,
-          }}
-        >
-          Edita Curs
-        </Button>
         <Button
           variant="outlined"
           sx={{
@@ -176,19 +175,38 @@ export default function ColorButtons() {
               borderColor: "red",
             },
             borderRadius: "16px",
-            width: 130,
+
+            width: "189.5px",
+            height: "44px",
           }}
         >
           Tanca
         </Button>
+        <LoadingButton
+          variant="contained"
+          sx={{
+            bgcolor: "#BED730",
+            color: "black",
+            borderRadius: "16px",
+            width: "189.5px",
+            height: "44px",
+          }}
+          type="submit"
+          loading={isLoading}
+        >
+          Desa nou curs
+        </LoadingButton>
       </Stack>
       <Box
-        component='form'
+        component="form"
         noValidate
-        autoComplete='off'
+        autoComplete="off"
         onSubmit={handleSubmit}
       >
-        <List direction="column" sx={{ color: "black", width: 300 }}>
+        <List
+          direction="column"
+          sx={{ color: "black", width: 300, borderRadius: "16px" }}
+        >
           Categoría
           <Stack sx={{ my: 1, bgcolor: "#E9F9FB", p: 2 }}>
             <RadioGroup
@@ -198,17 +216,29 @@ export default function ColorButtons() {
               value={selectedCategories}
             >
               {categories?.map((category) => (
-                <CategoriesRadios name='categories' key={category.name} value={category.id} label={category.name} />
+                <CategoriesRadios
+                  name="categories"
+                  key={category.name}
+                  value={category.id}
+                  label={category.name}
+                />
               ))}
             </RadioGroup>
           </Stack>
         </List>
         <List sx={{ color: "black" }}>
           Codi
-          <Stack sx={{ bgcolor: "#E9F9FB", alignItems: "center", my: 1 }}>
+          <Box
+            sx={{
+              bgcolor: "#E9F9FB",
+              alignItems: "center",
+              my: 1,
+              borderRadius: "16px",
+            }}
+          >
             <TextField
               placeholder="Escriu el codi del curs aquí"
-              sx={{ width: 300, }}
+              sx={{ width: "100%" }}
               id="demo-helper-text-aligned-no-helper"
               name="code"
               InputLabelProps={{
@@ -219,13 +249,20 @@ export default function ColorButtons() {
               value={form.code}
               onChange={handleChange}
             />
-          </Stack>
+          </Box>
         </List>
         <List sx={{ color: "black" }}>
           Titol
-          <Stack sx={{ bgcolor: "#E9F9FB", alignItems: "center", my: 2 }}>
+          <Stack
+            sx={{
+              bgcolor: "#E9F9FB",
+              alignItems: "center",
+              my: 2,
+              borderRadius: "16px",
+            }}
+          >
             <TextField
-              sx={{ width: 300, }}
+              sx={{ width: "100%" }}
               id="demo-helper-text-aligned-no-helper"
               placeholder="Escriu el títol del curs aquí"
               InputLabelProps={{
@@ -242,7 +279,14 @@ export default function ColorButtons() {
         <List sx={{ color: "black" }}>
           Seu
           <Stack
-            sx={{ flexGrow: 1, my: 1, bgcolor: "#E9F9FB", my: 2, width: 300 }}
+            sx={{
+              flexGrow: 1,
+              my: 1,
+              bgcolor: "#E9F9FB",
+              my: 2,
+              width: 300,
+              borderRadius: "16px",
+            }}
           >
             <RadioGroup
               row
@@ -277,18 +321,24 @@ export default function ColorButtons() {
         </List>
         <List sx={{ color: "black" }}>
           Hores Teoria
-          <Stack sx={{ bgcolor: "#E9F9FB", alignItems: "center", my: 2 }}>
+          <Box
+            sx={{
+              bgcolor: "#E9F9FB",
+              alignItems: "center",
+              my: 2,
+              borderRadius: "16px",
+            }}
+          >
             <Stack
               direction="row"
               sx={{
-                width: 300,
                 alignItems: "center",
                 justifyContent: "space-between",
               }}
             >
               <TextField
                 placeholder="800"
-                sx={{ width: 120 }}
+                sx={{ width: "100%" }}
                 id="demo-helper-text-aligned-no-helper"
                 InputLabelProps={{
                   style: { color: "black" },
@@ -301,21 +351,27 @@ export default function ColorButtons() {
                 onChange={handleChange}
               />
             </Stack>
-          </Stack>
+          </Box>
         </List>
         <List sx={{ color: "black" }}>
           Hores pràctica
-          <Stack sx={{ bgcolor: "#E9F9FB", alignItems: "center", my: 2 }}>
+          <Box
+            sx={{
+              bgcolor: "#E9F9FB",
+              alignItems: "center",
+              my: 2,
+              borderRadius: "16px",
+            }}
+          >
             <Stack
               direction="row"
               sx={{
-                width: 300,
                 alignItems: "center",
                 justifyContent: "space-between",
               }}
             >
               <TextField
-                sx={{ width: 120 }}
+                sx={{ width: "100%" }}
                 id="demo-helper-text-aligned-no-helper"
                 InputLabelProps={{
                   style: { color: "black" },
@@ -329,21 +385,28 @@ export default function ColorButtons() {
                 onChange={handleChange}
               />
             </Stack>
-          </Stack>
+          </Box>
         </List>
-       
+
         <List sx={{ color: "black" }}>
           Descripció
-          <Stack sx={{ bgcolor: "#E9F9FB", alignItems: "center", my: 2 }}>
+          <Stack
+            sx={{
+              bgcolor: "#E9F9FB",
+              alignItems: "center",
+              my: 2,
+              borderRadius: "16px",
+            }}
+          >
             <Stack
               sx={{
-                width: 300,
+                width: "100%",
                 alignItems: "center",
                 justifyContent: "space-between",
               }}
             >
               <TextField
-                sx={{ width: 300, }}
+                sx={{ width: "100%" }}
                 id="demo-helper-text-aligned-no-helper"
                 InputLabelProps={{
                   style: { color: "black" },
@@ -363,38 +426,40 @@ export default function ColorButtons() {
           justifyContent="center"
           direction="row"
           spacing={2}
-          sx={{ p: 2, m: 2, }}
+          sx={{ p: 2, m: 2 }}
         >
+          <Button
+            variant="outlined"
+            sx={{
+              borderColor: "#BED730",
+              color: "black",
+              "&:hover": {
+                borderColor: "red",
+              },
+              borderRadius: "16px",
 
+              width: "189.5px",
+              height: "44px",
+            }}
+          >
+            Tanca
+          </Button>
           <LoadingButton
-            variant='contained'
+            variant="contained"
             sx={{
               bgcolor: "#BED730",
               color: "black",
               borderRadius: "16px",
-              width: 130,
+              width: "189.5px",
+              height: "44px",
             }}
-            type='submit'
+            type="submit"
             loading={isLoading}
           >
-            Desa
+            Desa nou curs
           </LoadingButton>
-          <Button
-            variant="outlined"
-            sx={{
-              borderColor: "red",
-              color: "black",
-              "&:hover": {
-                borderColor: "#BED730",
-              },
-              borderRadius: "16px",
-              width: 130,
-            }}
-          >
-            Descarta
-          </Button>
         </Stack>
       </Box>
-    </Grid>
+    </Box>
   );
 }
